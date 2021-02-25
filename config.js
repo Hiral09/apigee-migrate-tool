f3e33109167e226b62a0b4c60363d4193394f2d8
hiral gopiyani <79524337+Hiral09@users.noreply.github.com>
2021-02-25 11:58:44 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 module.exports = {
-
-	from: {
-		version: '19.04',
-		url: 'https://login.apigee.com/login',
-		userid: 'gopiyanihiral@gmail.com',
-		passwd: 'Travel2018!',
-		org: 'org1',
-		env: 'test'
-	},
-	to: {
-		version: '19.04',
-		url: 'https://apigee.googleapis.com',
-		userid: 'hiral@digitaldisruptionllc.com',
-		passwd: 'Travel2021!',
-		org: 'Apigee X',
-		env: 'test'
-	}
-} ;
+    from: {
+                version: '1',
+                url: 'https://api.enterprise.apigee.com',
+                userid: 'gopiyanihiral@gmail.com',
+                passwd: 'Travel2018!',
+                org: 'gopiyanihiral-eval',
+                env: 'test'
+    },
+    to: {
+                version: '1',
+                url: 'https://apigee.googleapis.com',
+                userid: 'hiral@digitaldisruptionllc.com',
+                passwd: 'Travel2021!',
+                org: 'apigeex1',
+                env: 'my-test-environment'
+    }};
